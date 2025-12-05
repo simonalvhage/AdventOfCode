4490b2c4e7ba1dc30c8e8faadbe8aa9bc2009ddf
Simon Alvhage <simon.alvhage@gmail.com>
2025-12-05 06:47:20 +0100

    added day 5

--- 2024/setup.py
+++ 2024/setup.py
@@ -1,33 +1,37 @@
 import requests
 from datetime import date
 import os
-
-#"eyJkYjgzMTE1ZDM5ZTkwOTI5MWU5OTQ1M2VmM2MxN2JiYSI6eyJleHBpcmVkQXQiOjE3MzMwMzU4NzI1NDYsInZhbHVlIjoiMTNmZmQwZGQ2NzU1ZDhkNGEwMWUyNWM5YTJmMDc0YTcifX0="
+import shutil
+import helper_functions.explain
+import helper_functions.code
 
 def createfolder(today):
     print(today)
     if not os.path.exists("day" + str(today)):
         os.makedirs("day" + str(today))
         f = open("day" + str(today) + "/main.py", "a")
-        f.write("\ndef readfile():\n\twith open('input.txt', 'r') as k:\n\t\tfor items in k.read().splitlines():\n\t\t\tprint(items)\n\ndef main():\n\treadfile()\n\nif __name__ == '__main__':\n\tmain()")
+        shutil.copyfile("template.py", "day" + str(today) + "/main.py")
         f.close()
 
 def download_input(today):
-    uri = 'https://adventofcode.com/20244/day/'+ str(today) + '/input'
+    uri = 'https://adventofcode.com/2024/day/'+ str(today) + '/input'
     response = requests.get(uri, cookies={
-        'session': "53616c7465645f5fe233abb71092fb5980da9509390027d557c95f28612dfca79dd54ea801bd035daab1b825dbe0c3d702afa3c05528b7eb071c6f10182e6314"})
+        'session': "53616c7465645f5f1b16325f8898102035d6de1e57c880d1ea3cc7382bb1047c5d21720ac824a36357e4885c9693753143f773cbc5a3850bdcf1648d5f02c3f7"})
     if not os.path.exists("day" + str(today) + "/input.txt"):
         text_file = open("day" + str(today) + "/input.txt", "w")
         text_file.write(response.text)
         text_file.close()
 
 
-def main(today = None):
+def main(today = 9):
     if not today:
         today = date.today().strftime("%d")
         if int(today)<10: today = today.replace("0","")
     createfolder(today)
     download_input(today)
+
+    helper_functions.explain.main()
+    helper_functions.code.main()
 
 
 if __name__ == '__main__':
